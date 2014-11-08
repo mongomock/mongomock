f0a4e45224998df7b7b3693dfeaf026acaf2d20c
tipok <tipok88@mail.ru>
2014-11-08 16:53:34 +0100

    added dereference

--- tests/utils.py
+++ tests/utils.py
@@ -4,3 +4,10 @@ if sys.version_info < (2, 7):
     from unittest2 import TestCase, skipIf
 else:
     from unittest import TestCase, skipIf
+
+
+class DBRef(object):
+
+    def __init__(self, collection, id):
+        self.collection = collection
+        self.id = id
