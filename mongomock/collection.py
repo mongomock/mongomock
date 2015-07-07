986f47a51972029fdc817782a5301bf4fbd40eba
Alexandre Viau <alexandre@alexandreviau.net>
2015-07-07 19:09:15 -0400

    collection.remove: Accept kwargs

--- mongomock/collection.py
+++ mongomock/collection.py
@@ -518,7 +518,7 @@ class Collection(object):
                         manipulate, safe, _check_keys = True, **kwargs)
             return to_save.get("_id", None)
 
-    def remove(self, spec_or_id = None, search_filter = None):
+    def remove(self, spec_or_id = None, search_filter = None, **kwargs):
         """Remove objects matching spec_or_id from the collection."""
         if search_filter is not None:
             print_deprecation_warning('search_filter', 'spec_or_id')
