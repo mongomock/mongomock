eedd6f7dce57b30841bcaa10f25cc4ac3b314d57
Martin Domke <martin.domke@project-collins.com>
2015-08-17 20:59:38 +0200

    Add version and platform specifiers.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import os
-import itertools
 import platform
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), "mongomock", "__version__.py")) as version_file:
+
+version_file_path = os.path.join(
+    os.path.dirname(__file__), "mongomock", "__version__.py")
+
+
+with open(version_file_path) as version_file:
     exec(version_file.read())
+
 
 install_requires = ["sentinels", "six"]
 if platform.python_version() < '2.7':
@@ -13,14 +18,27 @@ if platform.python_version() < '2.7':
 
 if os.environ.get("INSTALL_PYMONGO", "false") == "true":
     install_requires.append("pymongo")
+
 if os.environ.get("INSTALL_PYEXECJS", "false") == "true":
     install_requires.append("pyexecjs")
 
 
 setup(name="mongomock",
-      classifiers = [
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
-          ],
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Topic :: Database"],
       description="Fake pymongo stub for testing simple MongoDB-dependent code",
       license="BSD",
       author="Rotem Yaari",
