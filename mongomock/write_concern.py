1ea2e565bafdf59ffbe10283129c99889140bc5c
Pascal Corpet <pascal@bayesimpact.org>
2018-08-08 14:33:08 +0200

    Implement a basic version of write_concern property. Fixes #406.

--- mongomock/write_concern.py
+++ mongomock/write_concern.py
@@ -1,3 +1,23 @@
 class WriteConcern(object):
     def __init__(self, w=None, wtimeout=None, j=None, fsync=None):
-        pass
+        self._document = {}
+        if w is not None:
+            self._document['w'] = w
+        if wtimeout is not None:
+            self._document['wtimeout'] = wtimeout
+        if j is not None:
+            self._document['j'] = j
+        if fsync is not None:
+            self._document['fsync'] = fsync
+
+    @property
+    def acknowledged(self):
+        return True
+
+    @property
+    def document(self):
+        return self._document.copy()
+
+    @property
+    def is_server_default(self):
+        return not self._document
