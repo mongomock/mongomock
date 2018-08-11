ceb4bc9471964b29a605d80f9f3feee3c362d23e
Henry Tang <henryykt@gmail.com>
2018-08-11 00:10:00 +0000

    Fix exception in python3 when aggregating with $group on composite _id. Fixes #419

--- mongomock/collection.py
+++ mongomock/collection.py
@@ -1664,7 +1664,8 @@ class Collection(object):
                     _id = stage['$group']['_id']
                     if _id:
                         key_getter = functools.partial(_parse_expression, _id)
-                        out_collection = sorted(out_collection, key=key_getter)
+                        sort_key_getter = _fix_sort_key(key_getter)
+                        out_collection = sorted(out_collection, key=sort_key_getter)
                         grouped = itertools.groupby(out_collection, key_getter)
                     else:
                         grouped = [(None, out_collection)]
@@ -1845,6 +1846,16 @@ def _resolve_sort_key(key, doc):
         return 0, value
 
     return 1, value
+
+
+def _fix_sort_key(key_getter):
+    def fixed_getter(doc):
+        key = key_getter(doc)
+        # Convert dictionaries to make sorted() work in Python 3.
+        if isinstance(key, dict):
+            return [(k, v) for (k, v) in sorted(key.items())]
+        return key
+    return fixed_getter
 
 
 class Cursor(object):
