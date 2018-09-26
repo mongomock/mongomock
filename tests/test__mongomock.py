f0d6698d71c7ef291d27a52c7afddbb804b03a58
Pascal Corpet <pascal@bayesimpact.org>
2018-09-26 23:57:34 +0200

    List skipped tests in CI and drop a test that is always skipped.

--- tests/test__mongomock.py
+++ tests/test__mongomock.py
@@ -1061,19 +1061,6 @@ class MongoClientCollectionTest(_CollectionComparisonTest):
         except Exception:
             assert False
 
-    def test__set_subdocuments(self):
-        """Tests using $set for setting subdocument fields"""
-        self.skipTest(
-            "MongoClient does not allow setting subdocuments on existing non-documents")
-        self.cmp.do.insert(
-            {'name': 'bob', 'data1': 1, 'subdocument': {'a': {'b': {'c': 20}}}})
-        self.cmp.do.update({'name': 'bob'}, {'$set': {'data1.field1': 11}})
-        self.cmp.compare.find()
-        self.cmp.do.update({'name': 'bob'}, {'$set': {'data2.field1': 21}})
-        self.cmp.compare.find()
-        self.cmp.do.update({'name': 'bob'}, {'$set': {'subdocument.a.b': 21}})
-        self.cmp.compare.find()
-
     def test__set_subdocuments_positional(self):
         self.cmp.do.insert({'name': 'bob', 'subdocs': [
             {'id': 1, 'name': 'foo'},
