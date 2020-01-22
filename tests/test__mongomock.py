1ce66535c4017c469137242d48b69b0ff6f2ebc9
Kostya Deev <kostya.deev@bluware.com>
2020-01-21 21:04:43 -0600

    better test coverage

--- tests/test__mongomock.py
+++ tests/test__mongomock.py
@@ -1035,11 +1035,17 @@ class MongoClientCollectionTest(_CollectionComparisonTest):
             self.cmp.compare.find({'name': 'Array'}, projection={
                 'name': 1, 'values': {'$slice': [-3, 10, 1]}})
 
-    def test__projection_slice_list_wrong_type(self):
+    def test__projection_slice_list_wrong_slice_type(self):
         self.cmp.do.insert({'name': 'Array', 'values': [0, 1, 2, 3, 4, 5, 6, 7]})
         with self.assertRaises(ValueError):
             self.cmp.compare.find({'name': 'Array'}, projection={
                 'name': 1, 'values': {'$slice': [1.0]}})
+
+    def test__projection_slice_list_wrong_value_type(self):
+        self.cmp.do.insert({'name': 'Array', 'values': 0})
+        with self.assertRaises(ValueError):
+            self.cmp.compare.find({'name': 'Array'}, projection={
+                'name': 1, 'values': {'$slice': 1}})
 
     def test__remove(self):
         """Test the remove method."""
