1db71fdda69d3d0cc4a7700981c424ba2da2c726
Jason Han <thestudentofkyoto@gmail.com>
2020-11-18 14:19:41 +0900

    behavior comparison test added

--- tests/test__mongomock.py
+++ tests/test__mongomock.py
@@ -719,6 +719,19 @@ class MongoClientCollectionTest(_CollectionComparisonTest):
         self.cmp.compare.find(
             {'field.0.a': {'$exists': True}, 'field.1.a': {'$exists': False}})
 
+    def test__find_in_array_equal_null(self):
+        self.cmp.do.insert_many([
+            {'_id': 1, 'shape': [{'color': 'red'}]},
+            {'_id': 2, 'shape': [{'color': 'yellow'}]},
+            {'_id': 3, 'shape': [{'color': 'red'}, {'color': 'yellow'}]},
+            {'_id': 4, 'shape': [{'size': 3}]},
+            {'_id': 5},
+            {'_id': 6, 'shape': {'color': ['red', 'yellow']}},
+        ])
+
+        self.cmp.compare_ignore_order.find({'shape.color': {'$eq': None}})
+        self.cmp.compare_ignore_order.find({'shape.color': None})
+
     def test__find_notequal(self):
         """Test searching with operators other than equality."""
         bob = {'_id': 1, 'name': 'bob'}
