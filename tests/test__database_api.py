d7c8004eed44c8380b5470232110f8bbaa7d71fd
Joshua Shaffer <joshua.shaffer@icmrl.net>
2022-07-14 14:11:24 -0400

    set uuid format on collection

--- tests/test__database_api.py
+++ tests/test__database_api.py
@@ -149,8 +149,10 @@ class DatabaseAPITest(TestCase):
             self.database.with_options(custom_document_class)
 
         custom_uuid_representation = codec_options.CodecOptions(uuid_representation=4)
-        db = self.database.with_options(custom_uuid_representation)
-        db.yes_hello.insert_one({'_id': uuid4()})
+        db = self.database
+        db.get_collection('yes_hello',
+                          codec_options=custom_uuid_representation).insert_one({'_id': uuid4()})
+        # db.yes_hello.insert_one({'_id': uuid4()})
 
         custom_unicode_error_hander = codec_options.CodecOptions(
             unicode_decode_error_handler='ignore')
