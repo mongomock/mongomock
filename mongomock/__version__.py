5cf154cc6c3e55d677d833345742969b3fe4c749
Matthew Evans <git@ml-evs.science>
2023-03-06 11:44:26 +0000

    Use importlib.metadata.version instead of deprecated pkg_resources call

--- mongomock/__version__.py
+++ mongomock/__version__.py
@@ -1,3 +1,10 @@
-import pkg_resources
+from platform import python_version_tuple
 
-__version__ = pkg_resources.get_distribution('mongomock').version
+python_version = python_version_tuple()
+
+if int(python_version[0]) >= 3 and int(python_version[1]) >= 8:
+    from importlib.metadata import version
+    __version__ = version('mongomock')
+else:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution('mongomock').version
