542229512377455a24da0da191e2170ef21a8af0
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-01-09 10:25:25 +0000

    Update mongomock/__version__.py
    Co-authored-by: AMHubert <48528470+AMHubert@users.noreply.github.com>

--- mongomock/__version__.py
+++ mongomock/__version__.py
@@ -2,7 +2,7 @@ from platform import python_version_tuple
 
 python_version = python_version_tuple()
 
-if (int(python_version[0]), and int(python_version[1])) >= (3, 8):
+if (int(python_version[0]), int(python_version[1])) >= (3, 8):
     from importlib.metadata import version
     __version__ = version('mongomock')
 else:
