875209cd169c2643b7b25b8e8228f6f46589e7d1
Thiago Salvatore <thiago@alby.com>
2025-03-07 17:49:02 -0300

    add support for comment on bulk_write

--- mongomock/collection.py
+++ mongomock/collection.py
@@ -2131,7 +2131,12 @@ class Collection:
             raise_not_implemented('session', 'Mongomock does not handle sessions yet')
         return self.database.rename_collection(self.name, new_name, **kwargs)
 
-    def bulk_write(self, requests, ordered=True, bypass_document_validation=False, session=None):
+    def bulk_write(
+        self, requests, ordered=True, bypass_document_validation=False, session=None, comment=None
+    ):
+        if comment:
+            warnings.warn('comment is ignored on mongomock.', stacklevel=2)
+
         if bypass_document_validation:
             raise NotImplementedError(
                 'Skipping document validation is a valid MongoDB operation;'
